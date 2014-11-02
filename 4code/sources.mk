60f3bc91ba6af2ff1a838130f322a172487a9664
balmer <balmer@inbox.ru>
2014-11-02 22:25:56 +0200

    - ????? ?????????? ?????? - ?????????? DelayUs, DelayMs. ????????? ?????? ? DelayUs ??-?? ??????? ?? ?????? ?? ??? ?????? (?????????? ? uint16_t) - ??? DAC ?????? ???????????? ???????, ?? ??? ???? ??? ???????. ???????, ??? ??????????.

---
+++ 4code/sources.mk
@@ -0,0 +1,109 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+
+DEPS += \
+	output/CMSIS/startup_stm32f4xx.d \
+	output/base/src/dac.d \
+	output/base/src/delay.d \
+	output/base/src/main.d \
+	output/base/src/stm32f4xx_it.d \
+	output/base/src/system_stm32f4xx.d \
+	output/StdPeriph_Driver/misc.d \
+	output/StdPeriph_Driver/stm32f4xx_dac.d \
+	output/StdPeriph_Driver/stm32f4xx_dma.d \
+	output/StdPeriph_Driver/stm32f4xx_exti.d \
+	output/StdPeriph_Driver/stm32f4xx_gpio.d \
+	output/StdPeriph_Driver/stm32f4xx_i2c.d \
+	output/StdPeriph_Driver/stm32f4xx_rcc.d \
+	output/StdPeriph_Driver/stm32f4xx_spi.d \
+	output/StdPeriph_Driver/stm32f4xx_syscfg.d \
+	output/StdPeriph_Driver/stm32f4xx_tim.d \
+
+
+OBJS += \
+	output/CMSIS/startup_stm32f4xx.o \
+	output/base/src/dac.o \
+	output/base/src/delay.o \
+	output/base/src/main.o \
+	output/base/src/stm32f4xx_it.o \
+	output/base/src/system_stm32f4xx.o \
+	output/StdPeriph_Driver/misc.o \
+	output/StdPeriph_Driver/stm32f4xx_dac.o \
+	output/StdPeriph_Driver/stm32f4xx_dma.o \
+	output/StdPeriph_Driver/stm32f4xx_exti.o \
+	output/StdPeriph_Driver/stm32f4xx_gpio.o \
+	output/StdPeriph_Driver/stm32f4xx_i2c.o \
+	output/StdPeriph_Driver/stm32f4xx_rcc.o \
+	output/StdPeriph_Driver/stm32f4xx_spi.o \
+	output/StdPeriph_Driver/stm32f4xx_syscfg.o \
+	output/StdPeriph_Driver/stm32f4xx_tim.o \
+
+
+output/CMSIS/startup_stm32f4xx.o: ../../SoundCard/code/Libraries/CMSIS/ST/STM32F4xx/Source/Templates/gcc_ride7/startup_stm32f4xx.S
+	@echo 'Building target: startup_stm32f4xx.S'
+	@$(CC) $(ASM_FLAGS) -o "$@" "$<"
+
+output/base/src/dac.o: ./src/dac.c
+	@echo 'Building target: dac.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/base/src/delay.o: ./src/delay.c
+	@echo 'Building target: delay.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/base/src/main.o: ./src/main.c
+	@echo 'Building target: main.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/base/src/stm32f4xx_it.o: ./src/stm32f4xx_it.c
+	@echo 'Building target: stm32f4xx_it.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/base/src/system_stm32f4xx.o: ./src/system_stm32f4xx.c
+	@echo 'Building target: system_stm32f4xx.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/misc.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/misc.c
+	@echo 'Building target: misc.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_dac.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_dac.c
+	@echo 'Building target: stm32f4xx_dac.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_dma.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_dma.c
+	@echo 'Building target: stm32f4xx_dma.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_exti.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_exti.c
+	@echo 'Building target: stm32f4xx_exti.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_gpio.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_gpio.c
+	@echo 'Building target: stm32f4xx_gpio.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_i2c.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_i2c.c
+	@echo 'Building target: stm32f4xx_i2c.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_rcc.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_rcc.c
+	@echo 'Building target: stm32f4xx_rcc.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_spi.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_spi.c
+	@echo 'Building target: stm32f4xx_spi.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_syscfg.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_syscfg.c
+	@echo 'Building target: stm32f4xx_syscfg.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+output/StdPeriph_Driver/stm32f4xx_tim.o: ../../SoundCard/code/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_tim.c
+	@echo 'Building target: stm32f4xx_tim.c'
+	@$(CC) $(C_FLAGS) -o "$@" "$<"
+
+
+
---
+++ 4code/src/main.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include "dac.h"
+#include "delay.h"
+
+RCC_ClocksTypeDef RCC_Clocks;
+
+int main(void)
+{  
+  /* SysTick end of count event each 1ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
+  /*
+  RCC_HSEConfig(RCC_HSE_ON);
+  while(!RCC_WaitForHSEStartUp())
+  {
+  }
+  */
+/*
+  DacTestInit();
+
+  while (1)
+  {
+  }
+*/
+  DelayInit();
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  GPIO_InitTypeDef gpio_A; 
+  gpio_A.GPIO_Pin  = GPIO_Pin_4;
+  gpio_A.GPIO_Mode = GPIO_Mode_OUT;
+  gpio_A.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  gpio_A.GPIO_Speed = GPIO_Speed_25MHz;
+  gpio_A.GPIO_OType = GPIO_OType_PP;
+  GPIO_Init(GPIOA, &gpio_A);
+
+
+  while (1)
+  {
+    GPIO_SetBits(GPIOA, GPIO_Pin_4);
+    //DelayUs(1000);
+    DelayMs(1);
+    GPIO_ResetBits(GPIOA, GPIO_Pin_4);
+    //DelayUs(1000);
+    DelayMs(1);
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+  
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
