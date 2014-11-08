9b9445774794f454ff1f2c329e4a310a28271741
balmer <balmer@inbox.ru>
2014-11-08 16:46:29 +0200

    - ???????? ? ????? PB12 ?? ????? PA4 ??????, ??????? ???????? ??????????. ? ?????????? ????? ??? ???????????? ??? ????, ????? ?????, ??? slave ?????????? ?????? ??? master.

--- 4code/src/spi.c
+++ 4code/src/spi.c
@@ -1,6 +1,7 @@
 #include "delay.h"
 #include "spi.h"
 /*
+	EXTI Slave 			 - PB12
 	SPI2_SCK             - PB13 34 *
 	SPI2_MISO            - PB14 35 *
 	SPI2_MOSI            - PB15 36 *
@@ -13,14 +14,20 @@ void SpiInit()
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;//GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource12, GPIO_AF_SPI2);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;//GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource13, GPIO_AF_SPI2);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, GPIO_AF_SPI2);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource15, GPIO_AF_SPI2);
@@ -43,10 +50,29 @@ void SpiInit()
 	SPI2->DR = 0x4000;
 }
 
+extern int g_sound_min;
+extern int g_sound_max;
+
+
 void SpiOnReceive()
 {
 	if (SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_RXNE)==SET) {
 		volatile uint16_t data = SPI2->DR; //Читаем то что пришло
+
+		if(g_sound_min <= g_sound_max)
+		{
+			data = g_sound_max-g_sound_min;
+			g_sound_min = 1<<24;
+			g_sound_max = -(1<<24);
+		}
+
 		SPI2->DR = data;
 	}
+}
+
+void SpiComplete()
+{
+	GPIO_SetBits(GPIOB, GPIO_Pin_12);
+	DelayUs(2);
+	GPIO_ResetBits(GPIOB, GPIO_Pin_12);
 }
